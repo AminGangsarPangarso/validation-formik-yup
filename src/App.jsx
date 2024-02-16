cab767497aa03f4d89a6e0ce871b70a29f6061b6
AminGangsarPangarso <amindailylife1976@gmail.com>
2024-02-16 14:27:50 +0700

    Feat: done form validation form formik dan yup

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,81 @@ import {
   Button,
   Container,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
   Input,
   Stack,
 } from "@chakra-ui/react";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 function App() {
+  const registerUser = () => {
+    alert(formik.values.password)
+    // alert("submit form");
+  };
+  const formik = useFormik({
+    initialValues: {
+      // isi dari initialvalues harus sama dengan name
+      username: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: registerUser,
+    validationSchema: yup.object().shape({
+      // required() bole bole tidak dipakai sesuai kebutuhan 
+      // isi validationscema harsu sama dengan initialvalue
+      username: yup.string().required().min(3).max(10),
+      email: yup.string().required().email(),
+      password: yup
+        .string()
+        .required()
+        .matches(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+          "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+        ),
+    }),
+  });
+
+  const handleform = (event) => {
+    formik.setFieldValue(event.target.name, event.target.value);
+  };
   return (
     <Container py="10">
       <Heading>Example Form</Heading>
       <Box padding="4" border="1px solid lightgray" borderRadius="4px" mt="8">
-        <form>
+        <form onSubmit={formik.handleSubmit}>
           <Stack spacing="3">
-            <FormControl>
+            <FormControl isInvalid={formik.errors.username}>
               <FormLabel>Username</FormLabel>
-              <Input />
+              <Input
+                onChange={handleform}
+                id="username"
+                name="username"
+                type="text"
+              />
+              <FormErrorMessage>{formik.errors.username}</FormErrorMessage>
             </FormControl>
-            <FormControl>
+            <FormControl isInvalid={formik.errors.email}>
               <FormLabel>Email</FormLabel>
-              <Input />
+              <Input
+                onChange={handleform}
+                type=" email"
+                id="email"
+                name="email"
+              />
+              <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
             </FormControl>
-            <FormControl>
+            <FormControl isInvalid={formik.errors.password}>
               <FormLabel>Password</FormLabel>
-              <Input />
+              <Input
+                onChange={handleform}
+                type="password"
+                id="password"
+                name="password"
+              />
+              <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
             </FormControl>
             <Button type="submit" colorScheme="teal">
               Register Account
